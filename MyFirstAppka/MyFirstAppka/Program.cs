cc71bd31b2cbe8cac73db22d4971450a4c1b0175
s27474 <s27474@pjwstk.edu.pl>
2024-03-08 15:19:41 +0100

    dodanie metody getAvarage i test

--- MyFirstAppka/MyFirstAppka/Program.cs
+++ MyFirstAppka/MyFirstAppka/Program.cs
@@ -6,4 +6,16 @@ Console.WriteLine("Hello, World!");
 
 // homo sum et nihil homonium a me alienum ese puto
 
+static double getAvarage(int[] lista)
+{
+    double result=0;
+    for (int i = 0; i < lista.Length; i++)
+    {
+        result += lista[i];
+    }
 
+    return result / lista.Length;
+}
+int[] paluszki = { 1, 5, 7, 8, 1234, 6, 2, 5, 2, 1, 4 };
+double cosik = getAvarage(paluszki);
+Console.WriteLine(cosik);
