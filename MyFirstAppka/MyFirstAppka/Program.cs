bec2cb32209636b8b3ca252cbcb905345b588fb8
s27474 <s27474@pjwstk.edu.pl>
2024-03-08 15:01:02 +0100

    dodanie komentarzy

--- MyFirstAppka/MyFirstAppka/Program.cs
+++ MyFirstAppka/MyFirstAppka/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+//fasting solution
+
+// homo sum et nihil homonium a me alienum ese puto
+
+
