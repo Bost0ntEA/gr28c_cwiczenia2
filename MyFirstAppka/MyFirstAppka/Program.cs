de2bc012894ca6d3aa90a568d1f1c75271164791
s27474 <s27474@pjwstk.edu.pl>
2024-03-13 20:08:20 +0100

    nowy branch z funkcja getMax

--- MyFirstAppka/MyFirstAppka/Program.cs
+++ MyFirstAppka/MyFirstAppka/Program.cs
@@ -19,3 +19,21 @@ static double getAvarage(int[] lista)
 int[] paluszki = { 1, 5, 7, 8, 1234, 6, 2, 5, 2, 1, 4 };
 double cosik = getAvarage(paluszki);
 Console.WriteLine(cosik);
+
+static int getMax(int[] tab)
+{
+    int max = tab[0];
+    for (int i = 1; i < tab.Length; i++)
+    {
+        if (tab[i]>max)
+        {
+            max = tab[i];
+        }
+    }
+
+    return max;
+}
+
+int[] mojaablica = {1,6,8,3,12,6,1,2,1,12};
+Console.WriteLine(getMax(mojaablica));
+Console.WriteLine();
